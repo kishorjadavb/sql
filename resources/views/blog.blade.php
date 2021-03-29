3ab91c3b02bf18c6574d933a9266b38385b602f7
kishorjadavb <kishore@digitalprizm.net>
2021-03-29 18:00:41 +0530

    blog view is updated

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -15,11 +15,11 @@
             <span class="inline-flex items-center justify-center h-6 px-3 text-white bg-green-600 rounded-full">{{session('mssg')}}</span> 
    
         @foreach ($blogPosts as $post )
-        <section class="p-5 text-indigo-200 bg-gray-900 body-font">
+        <section class="p-5 text-indigo-200 bg-gray-200 body-font">
          <Link to="coursedet">
         
-        <div class="flex flex-col items-center px-5 mx-auto md:flex-row jobcard">
-          <div class="flex flex-col items-center mb-16 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
+        <div class="flex flex-col items-center px-5 mx-auto bg-gray-200 md:flex-row jobcard">
+          {{-- <div class="flex flex-col items-center mb-16 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left md:mb-0">
             
             <figure class="visible">
           
@@ -54,11 +54,53 @@
           </div>
           <div class="hidden w-5/6 lg:max-w-lg lg:w-full md:w-1/2 sm:block">
             <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+          </div> --}}
+
+          <!--STAERT-->
+          <div>
+     <section class="text-gray-600 body-font">
+  <div class="container flex flex-col px-5 py-24 mx-auto">
+    <div class="mx-auto lg:w-4/6">
+      <div class="overflow-hidden rounded-lg h-74">
+        <img alt="content" class="object-cover object-center w-full h-84" src="{{$post->heading}}">
+      </div>
+      <div class="flex flex-col mt-10 sm:flex-row">
+        <div class="text-center sm:w-1/3 sm:pr-8 sm:py-8">
+          <div class="inline-flex items-center justify-center w-20 h-20 text-gray-400 bg-gray-200 rounded-full">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
           </div>
+          <div class="flex flex-col items-center justify-center text-center">
+            <h2 class="mt-4 text-lg font-medium text-gray-500 title-font">{{$post->description}}</h2>
+            <div class="w-12 h-1 mt-2 mb-4 bg-blue-500 rounded"></div>
+            <p class="text-base">{{$post->date}}.</p>
+          </div>
+        </div>
+        <div class="pt-4 mt-4 text-center border-t border-black sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l sm:border-t-0 sm:mt-0 sm:text-left">
+          <p class="mb-4 text-lg leading-relaxed text-gray-900">{{$post->content}}.</p>
+          <a class="inline-flex items-center text-blue-500">Learn More
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </a>
+          <a href="editPost/{{$post->id}}" class="inline-block px-2 py-1 mb-2 text-xs font-bold tracking-widest text-white bg-green-600 rounded-full">Edit</a>
+          <a href="deletePost/{{$post->id}}" class="inline-block px-2 py-1 mb-2 text-xs font-bold tracking-widest text-white bg-red-600 rounded-full">Delete</a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+</div>
+          <!--END-->
         </div>
         
       </section>
     @endforeach
+    
     {{-- @foreach ($blogPosts as $post )
             <div class="w-full max-w-md lg:flex">
         <div class="flex-none h-48 overflow-hidden text-center bg-cover rounded-t lg:h-auto lg:w-48 lg:rounded-t-none lg:rounded-l" style="background-image: url('https://tailwindcss.com/img/card-left.jpg')" title="Woman holding a mug">
